58a555b4ee34ee8a19d515a1e4e70603d4a4d679
larry-fuy <larry.yong.fu@gmail.com>
2014-12-20 23:50:36 -0600

    a minor change in provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,9 +4,9 @@ sudo apt-get update -y
 sudo apt-get -y curl git 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 curl -sSL https://get.docker.com/ubuntu/ | sudo sh
-git https://github.com/amplab/docker-scripts.git
+cd ~ && git clone https://github.com/amplab/docker-scripts.git
 
 # run spark clusters
-sudo ./deploy/deploy.sh -i amplab/spark:0.9.0 -w 3
+cd docker-scripts && sudo ./deploy/deploy.sh -i amplab/spark:1.0.0 -w 3
 
 # TODO: port forwarding (without change deploy script, iptable/socat)
